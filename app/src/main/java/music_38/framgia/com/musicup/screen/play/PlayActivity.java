163217a983428a467aa65197e7fd7fd2dc29b0d5
Hoanglong <>
2018-09-26 08:32:55 +0700

    2048-2050 RecyclerView Playlist

---
+++ app/src/main/java/music_38/framgia/com/musicup/screen/play/PlayActivity.java
@@ -0,0 +1,24 @@
+package music_38.framgia.com.musicup.screen.play;
+
+import android.os.Bundle;
+
+import music_38.framgia.com.musicup.R;
+import music_38.framgia.com.musicup.screen.base.BaseActivity;
+
+public class PlayActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.activity_play;
+    }
+
+    @Override
+    protected void initComponent() {
+
+    }
+
+    @Override
+    protected void initData(Bundle savedInstanceState) {
+
+    }
+}
